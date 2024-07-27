aa4072e1a30825acf217c6076fe48bb0b7a3e2a4
JangsanHighDev <mocakosan96@highdev.pro>
2024-07-27 17:48:54 +0900

    Add : Logo style in Sidebar

---
+++ components/elements/IconButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const IconButton = ({ icon, onClickIcon = () => {} }) => {
+  return (
+    <div>
+      <div
+        onClick={onClickIcon}
+        className="flex justify-center items-center w-[36px] h-[36px] hover:bg-[rgba(144,144,144,0.45)] rounded-full cursor-pointer"
+      >
+        {icon}
+        {/* <RxHamburgerMenu size={24} /> */}
+      </div>
+    </div>
+  );
+};
+
+export default IconButton;
--- components/elements/Logo.jsx
+++ components/elements/Logo.jsx
@@ -1,7 +1,33 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { RxHamburgerMenu } from "react-icons/rx";
+import IconButton from "./IconButton";
 
 const Logo = () => {
-  return <div>Logo</div>;
+  const { push } = useRouter();
+  const onClickLogo = () => {
+    //home으로 이동하는 로직
+    push("/");
+  };
+  const onClickMenu = () => {};
+  return (
+    <section className="flex flex-row items-center gap-3">
+      <IconButton
+        onClickIcon={onClickMenu}
+        icon={<RxHamburgerMenu size={24} />}
+      />
+      <div
+        className="flex items-center text-[30px] cursor-pointer"
+        onClick={onClickLogo}
+      >
+        <Image src={"/jsmusic.png"} width={30} height={30} alt="log" />
+        Music
+      </div>
+    </section>
+  );
 };
 
 export default Logo;
