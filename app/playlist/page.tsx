e237c15c29974ee43af884a09546ce7789661fa1
JangsanHighDev <mocakosan96@highdev.pro>
2024-07-29 21:48:53 +0900

    Add : playlisthead in playlist

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,7 +1,30 @@
+import { getPlaylistById } from "@/lib/dummyData";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { permanentRedirect } from "next/navigation";
 import React from "react";
+import HeaderBgChanger from "@/components/HeaderBgChanger";
+import PagePadding from "@/components/PagePadding";
+import PlayListHead from "@/components/PlayListHead";
 
-const page = (props: any) => {
-  return <div>PlayList {props.searchParams.list}</div>;
+interface PlaylistPageProps {
+  searchParams: {
+    list: string;
+  };
+}
+
+const page = async (props: PlaylistPageProps) => {
+  const playlist = await getPlaylistById(Number(props.searchParams.list));
+  if (!playlist) permanentRedirect("/");
+
+  const imageSrc = getRandomElementFromArray(playlist.songList)?.imageSrc;
+
+  return (
+    <PagePadding>
+      <HeaderBgChanger imageSrc={imageSrc} />
+      <div className="mt-12"></div>
+      <PlayListHead playlist={playlist} />
+    </PagePadding>
+  );
 };
 
 export default page;
