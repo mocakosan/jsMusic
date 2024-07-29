d15dcccdd48ba37c917a311f81da3a40abb6ace2
JangsanHighDev <mocakosan96@highdev.pro>
2024-07-29 23:12:07 +0900

    Add : channel Header in channel

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,7 +1,57 @@
+import HeaderBgChanger from "@/components/HeaderBgChanger";
+import PagePadding from "@/components/PagePadding";
+import DarkButton from "@/components/elements/DarkButton";
+import WhiteButton from "@/components/elements/WhiteButton";
+import { getChannelById } from "@/lib/dummyData";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { permanentRedirect } from "next/navigation";
 import React from "react";
+import { FiMusic, FiShuffle } from "react-icons/fi";
 
-const page = (props: any) => {
-  return <div>channel/[{props.params.id}]</div>;
+interface ChannelPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const page = async (props: ChannelPageProps) => {
+  const channel = await getChannelById(Number(props.params.id));
+
+  if (!channel) permanentRedirect("/");
+
+  const imageSrc = getRandomElementFromArray(channel.songList)?.imageSrc;
+
+  return (
+    <PagePadding>
+      <HeaderBgChanger imageSrc={imageSrc} />
+      <div className="mt-[150px]"></div>
+      <section>
+        <div className="text-[28px] font-bold">{channel.name}</div>
+        <article className="mt-4 lg:hidden">
+          <div>
+            <DarkButton
+              className={"w-[230px] flex justify-center"}
+              label={"구독중 99만"}
+            />
+          </div>
+          <div className="flex flex-row gap-4 mt-4">
+            <WhiteButton label={"셔플"} icon={<FiShuffle size={16} />} />
+            <WhiteButton label={"뮤직 스테이션"} icon={<FiMusic size={16} />} />
+          </div>
+        </article>
+        <div className="hidden lg:flex flex-row items-center gap-4 text-[14px] mt-4">
+          <WhiteButton label={"셔플"} icon={<FiShuffle size={16} />} />
+          <WhiteButton label={"뮤직 스테이션"} icon={<FiMusic size={16} />} />
+          <DarkButton
+            className={"w-[230px] flex justify-center"}
+            label={"구독중 99만"}
+          />
+        </div>
+      </section>
+      <section>노래</section>
+      <section>앨범</section>
+    </PagePadding>
+  );
 };
 
 export default page;
